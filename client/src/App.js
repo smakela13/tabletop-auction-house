4a362adbb86a3579243cffdd35923df4b71177c5
SMakela <73409799+smakela13@users.noreply.github.com>
2022-03-02 20:35:37 -0500

    Added back GraphQL - Removed unneeded files

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
 import React, {useEffect} from 'react';
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navbar';
 import Product from './pages/store';
@@ -12,11 +19,34 @@ import Footer from './components/Footer';
 import SingleProduct from './pages/SingleProduct';
 import { keepTheme } from './components/themes';
 
+const httpLink = createHttpLink({
+	uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+	// get the authentication token from local storage if it exists
+	const token = localStorage.getItem('id_token');
+	// return the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
+
+const client = new ApolloClient({
+	// Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
+
 export default function App() {
 	useEffect(() => {
 		keepTheme();
 	})
 	return (
+		<ApolloProvider client={client}>
 		<Router>
 			<Routes>
 				<div className='impDiv'>
@@ -47,8 +77,9 @@ export default function App() {
 						<SingleProduct />
 					</Route>
 					<Footer />
-			</div>
+				</div>
 			</Routes>
 		</Router>
+		</ApolloProvider>
 	);
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const compression = require('compression');
-const routes = require('./routes');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: authMiddleware,
+});
 
 server.applyMiddleware({ app });
 
@@ -15,13 +23,19 @@ app.use(compression());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`We'll leave as soon as you're ready, Docking Bay ${PORT}!`);
-  });
+	app.listen(PORT, () => {
+		console.log(`We'll leave as soon as you're ready, Docking Bay ${PORT}!`);
+		// log where we can go to test our GQL API
+		console.log(
+			`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+		);
+	});
 });
